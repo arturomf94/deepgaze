4b8caa4ff819483b61196f4825949f19453d4eb9
arturomf94 <arturomf94@gmail.com>
2018-02-24 13:47:31 -0600

    test skin detection

--- examples/ex_skin_detection_images/ex_skin_detection_images.py
+++ examples/ex_skin_detection_images/ex_skin_detection_images.py
@@ -14,6 +14,9 @@
 #the detector with the right morphing operation in order to have clean results
 #and remove noise. The filter use HSV color representation (https://en.wikipedia.org/wiki/HSL_and_HSV)
 
+import sys
+sys.path[0] = "/usr/local/lib/python2.7/dist-packages"
+
 import numpy as np
 import cv2
 from deepgaze.color_detection import RangeColorDetector
@@ -22,16 +25,16 @@ from deepgaze.color_detection import RangeColorDetector
 min_range = np.array([0, 48, 70], dtype = "uint8") #lower HSV boundary of skin color
 max_range = np.array([20, 150, 255], dtype = "uint8") #upper HSV boundary of skin color
 my_skin_detector = RangeColorDetector(min_range, max_range) #Define the detector object
-image = cv2.imread("tomb_rider.jpg") #Read the image with OpenCV
+image = cv2.imread("rodo.jpg") #Read the image with OpenCV
 #We do not need to remove noise from this image so morph_opening and blur are se to False
 image_filtered = my_skin_detector.returnFiltered(image, morph_opening=False, blur=False, kernel_size=3, iterations=1)
-cv2.imwrite("tomb_rider_filtered.jpg", image_filtered) #Save the filtered image
+cv2.imwrite("rodo_filtered.jpg", image_filtered) #Save the filtered image
 
-#Second image boundaries
-min_range = np.array([0, 58, 50], dtype = "uint8") #lower HSV boundary of skin color
-max_range = np.array([30, 255, 255], dtype = "uint8") #upper HSV boundary of skin color
-image = cv2.imread("tomb_rider_2.jpg") #Read the image with OpenCV
-my_skin_detector.setRange(min_range, max_range) #Set the new range for the color detector object
-#For this image we use one iteration of the morph_opening and gaussian blur to clear the noise
-image_filtered = my_skin_detector.returnFiltered(image, morph_opening=True, blur=True, kernel_size=3, iterations=1)
-cv2.imwrite("tomb_rider_2_filtered.jpg", image_filtered) #Save the filtered image
+# #Second image boundaries
+# min_range = np.array([0, 58, 50], dtype = "uint8") #lower HSV boundary of skin color
+# max_range = np.array([30, 255, 255], dtype = "uint8") #upper HSV boundary of skin color
+# image = cv2.imread("tomb_rider_2.jpg") #Read the image with OpenCV
+# my_skin_detector.setRange(min_range, max_range) #Set the new range for the color detector object
+# #For this image we use one iteration of the morph_opening and gaussian blur to clear the noise
+# image_filtered = my_skin_detector.returnFiltered(image, morph_opening=True, blur=True, kernel_size=3, iterations=1)
+# cv2.imwrite("tomb_rider_2_filtered.jpg", image_filtered) #Save the filtered image
