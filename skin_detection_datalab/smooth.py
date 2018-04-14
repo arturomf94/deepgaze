9a21c02a8b7dc6158df5965872107097357cbc1c
arturomf94 <arturomf94@gmail.com>
2018-04-14 14:07:48 -0500

    add smothing script - needs development

---
+++ skin_detection_datalab/smooth.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('results/face_filtered_robin2.jpg')
+
+kernel = np.ones((25,25),np.float32)/25
+dst = cv2.filter2D(img,-1,kernel)
+
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+blur = cv2.blur(img,(25,25))
+
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+blur = cv2.GaussianBlur(img,(25,25),5)
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
+
+blur = cv2.bilateralFilter(img,18,175,175)
+plt.subplot(121),plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(blur),plt.title('Blurred')
+plt.xticks([]), plt.yticks([])
+plt.show()
